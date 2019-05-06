8224e27da5b1d20de5e18f4491829c02fe9eb515
Mark May <mamark90@gmail.com>
2019-05-06 10:35:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,72 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+
+  dictionary_hash = {  "hello" => "hi",
+       "to" => "2",
+       "two" => "2",
+       "too" => "2",
+       "for" => "4",
+       "four" => "4",
+       "For" => "4",
+       "be"  => "b",
+       "you" => "u",
+       "at"  => "@",
+       "and" => "&"
+    }
+end
+
+def word_substituter(tweet)
+
+  dictionary_hash = dictionary
+  tweet = tweet.split
+
+  tweet_array = []
+  tweet.each do |x|
+
+    if dictionary_hash.include?(x)
+      value = dictionary_hash.fetch(x)
+      x = value
+      tweet_array << x
+    else
+    tweet_array << x
+    end
+
+  end
+tweet_array.join (" ")
+
+end
+
+
+def bulk_tweet_shortener(tweets)
+
+array = tweets
+
+array.each do |s|
+
+puts word_substituter(s)
+
+end
+
+end
+
+
+def selective_tweet_shortener(tweets)
+
+  array = tweets
+
+  array.each do |s|
+
+  string_characters = word_substituter(s)
+
+    if string_characters.length < 141
+      return string_characters
+
+    else
+        string_characters.slice!(0,140)
+
+  end
+
+end
+
+end
